d34f54dbe5872d99208314121b79a7439ba0476d
Austin F <austinfujimori123@gmail.com>
2020-09-03 13:49:57 -0700

    hellohello

--- Change.java
+++ Change.java
@@ -5,9 +5,7 @@ public class Change{
      }
      // Count the number of ways to make change for a give amount of money
      // Hard coded to 1, 5, 10, and 25
-     // Number of ways to make 6 cents
 
      public static void int numberOfWaysToMakeChange(int cents){
-          
      }
 }
